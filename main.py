b58db40755003746cd65f7da4466a4c167efa842
Rodrigo Fernandes <digonalha@gmail.com>
2021-10-12 16:12:14 -0300

    [add] dockerfile; [fix] melhorando fluxo de inicialização da aplicação; [update] requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import threading
+import time
+import os
+from src.helpers.logging_helper import SystemLogging
 from src.repositories.database import database
 from src.services.app import api_listener_service, sale_tracker_service
 from src.services import (
@@ -9,24 +12,53 @@ from src.services import (
     keyword_service,
 )
 
-print("→ starting telegrambot")
+main_syslog = SystemLogging(__name__)
 
-# create tables if not exists
-print("→ creating tables if not exists... ", end="")
-database.create_tables()
-print("done!")
 
-# get current users on database
-print("→ get all objects from tables... ", end="")
-user_service.get_all_users()
-moderator_service.get_all_moderators()
-custom_command_service.get_all_commands()
-tracked_sale_service.get_all_tracked_sales()
-keyword_service.get_all_keywords()
-print("done!")
+def cls():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def start_bot(attempts: int = 0):
+    total_attempts = attempts
+    try:
+        cls()
+        print("→ starting telegrambot")
+
+        # create tables if not exists
+        print("→ creating tables if not exists... ", end="")
+        database.create_tables()
+        print("done!")
+
+        # get current users on database
+        print("→ get all objects from tables... ", end="")
+        user_service.get_all_users()
+        moderator_service.get_all_moderators()
+        custom_command_service.get_all_commands()
+        tracked_sale_service.get_all_tracked_sales()
+        keyword_service.get_all_keywords()
+        print("done!")
+    except Exception as ex:
+        if total_attempts <= 3:
+            total_attempts += 1
+            time_in_seconds = total_attempts * 10
+            print(
+                f"an error occurred. waiting {time_in_seconds} seconds to try again..."
+            )
+            time.sleep(time_in_seconds)
+            start_bot(total_attempts)
+        else:
+            main_syslog.create_warning("start_bot", ex)
+            print("could not start application. please check the log file")
+            exit()
 
 
 def main():
+    try:
+        start_bot()
+    except:
+        time.sleep(10)
+
     print("→ starting sale tracker thread... ", end="")
     t1 = threading.Thread(target=sale_tracker_service.run_sale_tracker)
     t1.start()
