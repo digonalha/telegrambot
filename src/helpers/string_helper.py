c59eef41dab1b20e891f09e639f28aadcccff269
Rodrigo Fernandes <digonalha@gmail.com>
2021-11-28 12:51:45 -0300

    include <b> on html sanitize and &amp

--- src/helpers/string_helper.py
+++ src/helpers/string_helper.py
@@ -64,16 +64,20 @@ def format_decimal(str_currency) -> str:
 
 def html_sanitize(str_html: str) -> str:
     html_sanitized = str_html.replace("<br>", "\n")
-
+    html_sanitized = str_html.replace("</br>", "\n")
     html_sanitized = html_sanitized.replace("<br >", "\n")
     html_sanitized = html_sanitized.replace("<br/>", "\n")
     html_sanitized = html_sanitized.replace("<br />", "\n")
     html_sanitized = html_sanitized.replace("&nbsp;", " ")
+    html_sanitized = html_sanitized.replace("&amp;", " ")
     html_sanitized = html_sanitized.replace("\n\n\n", "\n\n")
     html_sanitized = html_sanitized.replace("\n\n\n\n", "\n\n")
     html_sanitized = html_sanitized.replace("\n\n\n\n\n", "\n\n")
+    html_sanitized = str_html.replace("<b>", "\n")
+    html_sanitized = str_html.replace("</b>", "\n")
 
     return html_sanitized
+
 
 def string_sanitize(text: str) -> str:
     text = text.replace('"', "")
