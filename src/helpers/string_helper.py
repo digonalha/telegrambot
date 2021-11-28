ac05e3ec04a3162029a3ad571c1fa70adebb6a57
Rodrigo Fernandes <digonalha@gmail.com>
2021-11-28 14:34:52 -0300

    fix

--- src/helpers/string_helper.py
+++ src/helpers/string_helper.py
@@ -64,6 +64,7 @@ def format_decimal(str_currency) -> str:
 
 def html_sanitize(str_html: str) -> str:
     html_sanitized = str_html.replace("<br>", "\n")
+
     html_sanitized = html_sanitized.replace("</br>", "\n")
     html_sanitized = html_sanitized.replace("<br >", "\n")
     html_sanitized = html_sanitized.replace("<br/>", "\n")
@@ -72,8 +73,6 @@ def html_sanitize(str_html: str) -> str:
     html_sanitized = html_sanitized.replace("\n\n\n", "\n\n")
     html_sanitized = html_sanitized.replace("\n\n\n\n", "\n\n")
     html_sanitized = html_sanitized.replace("\n\n\n\n\n", "\n\n")
-    html_sanitized = html_sanitized.replace("<b>", "\n")
-    html_sanitized = html_sanitized.replace("</b>", "\n")
 
     return html_sanitized
 
