3fc464641a59a342422cb72d7288e2418ef3e8fd
Rodrigo Fernandes <digonalha@gmail.com>
2022-03-30 13:45:10 -0300

    correção

--- src/domain/services/tracking_event_service.py
+++ src/domain/services/tracking_event_service.py
@@ -91,7 +91,7 @@ def list_tracking_events(code, list_all=True):
             if tracking_event:
                 tracking_events.append(tracking_event)
 
-    tracking_message = f"Nenhum evento encontrado para o código de rastreio <b>{code.tracking_code}</b>"
+    tracking_message = ''
 
     if len(tracking_events) > 0:
         if code.name:
@@ -148,5 +148,8 @@ def list_tracking_events(code, list_all=True):
 
         if len(tracking_info) > 1:
             tracking_message += f"\n\n******\n<b>Tempo decorrido</b>: \n{days_between(tracking_info[len(tracking_info) - 1]['dtHrCriado'], tracking_info[0]['dtHrCriado'])} dias"
+    elif list_all:
+        tracking_message = f"Nenhum evento encontrado para o código de rastreio <b>{code.tracking_code}</b>"
 
-    message_service.send_message(code.user_id, tracking_message, parse_mode="HTML")
+    if tracking_message:
+        message_service.send_message(code.user_id, tracking_message, parse_mode="HTML")
