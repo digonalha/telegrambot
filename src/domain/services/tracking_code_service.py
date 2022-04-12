bbb8f8c2c05b12de61cb5cc8259ca36707601934
Rodrigo Fernandes <digonalha@gmail.com>
2022-04-12 11:55:32 -0300

    fix header rastreio

--- src/domain/services/tracking_code_service.py
+++ src/domain/services/tracking_code_service.py
@@ -154,7 +154,7 @@ def get_user_trackings(user_id: int) -> list:
                 if len(tc.event) > 0:
                     str_event = tc.event[len(tc.event) - 1].description
 
-                message += f"\n{emoji}  <code>{tc.tracking_code}</code> {'' if not tc.name else ' [' + tc.name + ']'}"
+                message += f"\n{emoji}  {'' if not tc.name else ' [' + tc.name + ']'}"
                 message += f"\n{str_event}"
                 message += f"\n<code>/rastreio {tc.tracking_code}</code>\n"
 
